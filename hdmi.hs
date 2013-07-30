7f0a27824734ba970faae925efe269bd4d6d2b02
Vincent Foley <vfoley@gmail.com>
2013-07-29 20:53:01 -0400

    Initial commit

---
+++ hdmi.hs
@@ -0,0 +1,46 @@
+module Main where
+
+import Control.Monad (liftM)
+import System.Environment (getArgs, getProgName)
+import System.IO (stderr, hPutStrLn)
+import System.Process (system)
+
+data Hdmi = HdmiOn | HdmiOff
+
+{-| Return the shell commands to execute when turning HDMI on/off.
+  Commands include extending the display with xrandr, and moving
+  sound output to HDMI1.
+ -}
+-- TODO: Add commands to change pulseaudio output device.
+hdmiCommand :: Hdmi -> [String]
+hdmiCommand HdmiOn  = ["xrandr --output HDMI1 --auto --right-of LVDS1"]
+hdmiCommand HdmiOff = ["xrandr --output HDMI1 --off --right-of LVDS1"]
+
+{-| Read the command line parameter and return either a Maybe Hdmi
+    if "on" or "off" are provided, and Nothing otherwise. -}
+getHdmiFlag :: [String] -> Maybe Hdmi
+getHdmiFlag ["on"]  = Just HdmiOn
+getHdmiFlag ["off"] = Just HdmiOff
+getHdmiFlag _       = Nothing
+
+
+{-| Execute the given shell commands -}
+-- TODO: See if it's possible to directly call the xrandr and pulseaudio
+--       APIs instead of shelling out.
+runCommands :: [String] -> IO ()
+runCommands commands = mapM_ system commands
+
+
+{-| Display a usage string -}
+usage :: IO ()
+usage = do
+    prog <- getProgName
+    hPutStrLn stderr $ "Usage: " ++ prog ++ " <on|off>"
+
+
+main :: IO ()
+main = do
+  hdmiMaybe <- getHdmiFlag `liftM` getArgs
+  maybe usage
+        (runCommands . hdmiCommand)
+        hdmiMaybe
